d1c914df84e7ae5cb7aa0309522ef186ef45b794
cohadar <cohadar@gmail.com>
2016-05-10 09:30:29 +0200

    first step

---
+++ src/main/java/org/cohadar/mighty_sort/App.java
@@ -0,0 +1,13 @@
+package org.cohadar.mighty_sort;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
